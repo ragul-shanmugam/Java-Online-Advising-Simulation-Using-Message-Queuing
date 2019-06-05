cb3d69d54575de3131ed3c332e738b0bed169d94
Ragul Shanmugam <ragul.shanmugam@mavs.uta.edu>
2019-06-05 11:22:04 -0500

    First commit

---
+++ MQInterface.java
@@ -0,0 +1,28 @@
+/*
+Name: Ragul Shanmugam
+Student ID: 1001657250
+*/
+
+import java.util.HashMap;
+
+/*Interface class to store the messages if the server went down 
+and read it back when it is back up*/
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ragul
+ */
+
+public class MQInterface {
+    
+    public HashMap<String, String> studentCourseDetails = new HashMap<String, String>();
+    
+    public HashMap<String, String> advisorDecisionDetails = new HashMap<String, String>();
+    
+}
